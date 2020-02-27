8fcbe84ef5cfabd8cb33a79b5678895bd96fbe93
Greg Smith <18602791+chasinggreg@users.noreply.github.com>
2020-02-27 10:16:17 -0600

    Update constants.py

--- constants.py
+++ constants.py
@@ -3,4 +3,38 @@
 
 """Dictionary of constants used throughout the application."""
 
-# 
+
+# API Regions URLS
+ru_api = 'https://api.worldoftanks.eu/wot/'
+eu_api = 'https://api.worldoftanks.eu/wot/'
+na_api = 'https://api.worldoftanks.com/wot/'
+asia_api = 'https://api.worldoftanks.asia/wot/' 
+
+
+# API Categories
+## Accounts
+account_list = 'account/list/?'
+account_info = 'account/info/?'
+account_tanks = 'account/tanks/?'
+account_achievements = 'account/achievements/?'
+
+## Clan ratings
+
+## Player's vehicles
+
+## Clans
+clans_list = 'clans/list/?'
+clans_info = 'clans/info/?'
+clans_accountinfo = 'clans/accountinfo/?'
+player_clan_history = 'clans/memberhistory/?'
+
+
+# Mark of Mastery Indicators
+mark_of_mastery
+
+# mastery badges
+0 = 'None'
+1 = '3rd Class'
+2 = '2nd Class'
+3 = '1st Class'
+4 = 'Ace Tanker'
